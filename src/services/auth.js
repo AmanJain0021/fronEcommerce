65ddba083f545dbde480a0af196b6a6a7d5b7fe5
AmanJain0021 <amanjain4691@gmail.com>
2026-01-27 19:35:46 +0530

    Fix login API endpoint

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,9 @@
 import api from "./api";
 
 export const registerUser = (data) => {
-  return api.post("/users/register", data);
+  return api.post("/api/users/register", data);
 };
 
 export const loginUser = (data) => {
-  return api.post("/users/login", data);
+  return api.post("/api/users/login", data);
 };
